7bde78bb93cbdc797f343d63e336a5e3c9b3d7c7
cameronporter59 <35824363+cameronporter59@users.noreply.github.com>
2018-01-26 22:07:02 -0600

    Add files via upload

---
+++ repository.c
@@ -0,0 +1,16 @@
+/* 
+* Author: Cameron Porer
+* Date: 1/26/2018
+*
+* This program prints my name, and after being put on github,
+* my partner's name as well
+*/
+
+#include <stdlib.h>
+
+int main(char **argv){
+	printf("I'm Cameron");
+	
+	
+	return 0;
+}
